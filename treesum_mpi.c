da3beae45def8427a08d8555b499404189c4ee8d
Zihao Zhang <zihaozhang@umail.ucsb.edu>
2019-10-26 00:38:48 -0700

    supposedly done

--- treesum_mpi.c
+++ treesum_mpi.c
@@ -28,21 +28,28 @@ int global_sum(int my_int /* in */, int my_rank /* in */, int comm_sz /* in */,
   MPI_Status status;
   int result;
 
+  printf("I am proc %d, comm size = %d \n", my_rank, comm_sz);
+
   if (comm_sz <= 0 || my_rank < 0 || my_rank >= comm_sz)
     return 0;
-   printf("comm size = %d \n", comm_sz);
+  
   if(my_rank % 2 !=0 && my_rank > 0){
-    printf("sending myrank = %d \n", my_rank);
+    //printf("1sending, i am proc %d \n", my_rank);
     MPI_Send (&my_sum, 1, MPI_INT, my_rank -1, 0, comm);
   }
   for( int gap = 1; gap < comm_sz; gap = gap * 2){
-    if(my_rank % (2 * gap) == 0 && my_rank + gap < comm_sz ){
-      printf("gap = %d  myrank = %d \n", gap, my_rank);
-      MPI_Recv(&other_sum, 1, MPI_INT, my_rank + gap, 0, comm, &status);
-      my_sum= other_sum + my_sum;
-      if((my_rank % (4 * gap)) != 0 && my_rank >= 2 * gap)
-        printf("sending myrank = %d \n", my_rank);
+    //printf("gap = %d  myrank = %d \n", gap, my_rank);
+    if(my_rank % (2 * gap) == 0 ){
+      //printf("receiving, i am proc %d \n", my_rank);
+      if(my_rank + gap < comm_sz){
+        MPI_Recv(&other_sum, 1, MPI_INT, my_rank + gap, 0, comm, &status);
+        my_sum= other_sum + my_sum;
+        //printf("sum = %d, i am proc %d \n", my_sum, my_rank);
+      }
+      if((my_rank % (4 * gap)) != 0 && my_rank >= 2 * gap){
+        //printf("2sending, i am proc %d \n", my_rank);
         MPI_Send (&my_sum, 1, MPI_INT,  my_rank - 2 * gap, 0, comm);
+      }
     }
   }
   return my_sum;
